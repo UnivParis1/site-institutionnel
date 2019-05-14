fd9bdd4bb1999f9c0451a661327bbe039cb5336d
SLA11167 <stephane.lameul@open-groupe.com>
2019-05-14 12:09:10 +0200

    Module pour les annuaires des micro site

--- web/modules/custom/micro_annuaire_sorbonne/src/Controller/AnnuaireController.php
+++ web/modules/custom/micro_annuaire_sorbonne/src/Controller/AnnuaireController.php
@@ -20,39 +20,47 @@ class AnnuaireController extends ControllerBase {
   }
 
   /**
-   * List.
+   * Retourne une listre triée par ordre alphabétique et filtrée en fonction de la lettre
+   * de l'alphabet demandée.
    *
-   * @return string
-   *   Return Hello string.
+   * @param $letter lettre de l'alphabet dont on veut la page d'annuaire
+   * @return render array
+   *   Contenant la liste des utilisateurs sous forme de tableau (JSON_decode).
    */
   public function list($letter) {
     $cache = \Drupal::cache();
-
-    $negotiator = \Drupal::service('micro_site.negotiator');
-    $currentSiteId = $negotiator->getActiveId();
-
-    $users = [];
-    if (!empty($currentSiteId)) {
-      $cachedUser = $cache->get('users_ldap');
-      if ($cachedUser){
-        $users = $cachedUser->data;
-      }
-      else {
-        $users = $this->annuaireService->getUserList($currentSiteId);
-        $cache->set('users_ldap', $users, time() + 60*60);
-      }
-    }
-
     $filtered_users = [];
-    foreach ($users as $user){
-      if (strcasecmp(substr($user['sn'],0,1),$letter) == 0){
-        $filtered_users[] = $user;
+    $currentSiteId = '';
+
+    /** @var $negotiator  \Drupal\micro_site\SiteNegotiatorInterface */
+    $negotiator = \Drupal::service('micro_site.negotiator');
+//    dump($negotiator->getActiveSite());
+    if (!empty($negotiator->getActiveSite())) {
+      $currentSiteId = $negotiator->getActiveId();
+
+      $users = [];
+      if (!empty($currentSiteId)) {
+        // On verifie si l'annuaire est dans le cache
+        $cachedUser = $cache->get('users_ldap');
+        if ($cachedUser) {
+          $users = $cachedUser->data;
+        }
+        else {
+          $users = $this->annuaireService->getUserList($currentSiteId);
+          $cache->set('users_ldap', $users, time() + 60 * 60);
+        }
       }
+
+      foreach ($users as $user) {
+        if (strcasecmp(substr($user['sn'], 0, 1), $letter) == 0) {
+          $filtered_users[] = $user;
+        }
+      }
+      // on trie les utilisateurs par ordre alphabetique des cn
+      $sortedUsers = usort($filtered_users, function ($a, $b) {
+        return strnatcasecmp($a['sn'], $b['sn']);
+      });
     }
-    // on trie les utilisateurs par ordre alphabetique des cn
-    $sortedUsers = usort($filtered_users, function ($a, $b) {
-      return strnatcasecmp($a['sn'], $b['sn']);
-    });
 
     $build['item_list'] = [
       '#theme' => 'micro_annuaire_sorbonne',
