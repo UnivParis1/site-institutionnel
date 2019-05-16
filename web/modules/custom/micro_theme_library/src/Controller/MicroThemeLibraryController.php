871286e3ab8dffddf971656f0f2ae0df9396e919
SLA11167 <stephane.lameul@open-groupe.com>
2019-05-16 14:40:42 +0200

    Creation du module micro_theme_library pour que le webmestre sous-site puisse choisir/modifier le theme de son sous site

---
+++ web/modules/custom/micro_theme_library/src/Controller/MicroThemeLibraryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\micro_theme_library\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class MicroThemeLibraryController.
+ */
+class MicroThemeLibraryController extends ControllerBase {
+
+  /**
+   * Action supplementaire lors de la soumission du formulaire de config du theme d'un mini site
+   * => on enregistre le theme selectionné dans l'entité mini site
+   *
+   * @param $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   */
+  public static function form_submit(&$form, FormStateInterface $form_state) {
+//    dump($form_state);
+//    die();
+    $theme = $form_state->getUserInput()['theme']['general_theme'];
+    if (!empty($theme)) {
+      $siteStorage = \Drupal::entityTypeManager()->getStorage('site');
+      $currentSite = $siteStorage->load($form_state->getUserInput()['site_id']);
+      $currentSite->set('theme', ($theme == '_none' ? NULL : $theme));
+      $currentSite->save();
+    }
+  }
+
+}
