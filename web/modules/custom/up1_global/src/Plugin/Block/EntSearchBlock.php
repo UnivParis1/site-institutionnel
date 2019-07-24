b5f6b631f0ca42e773ac702cec7d5f3985ea4e40
lhelmany <Liliana.Helmany@univ-paris1.fr>
2019-07-24 11:20:41 +0200

    Add ENT block in up1_global module

---
+++ web/modules/custom/up1_global/src/Plugin/Block/EntSearchBlock.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\up1_global\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a "Ent & Search" block.
+ * @Block(
+ *   id="up1_ent_search_block",
+ *   admin_label = @Translation("ENT Search block"),
+ *   category = @Translation("Panthéon-Sorbonne"),
+ * )
+ *
+ * @package Drupal\up1_global\Plugin\Block
+ */
+class EntSearchBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+
+    return [
+      '#theme' => 'up1_ent_search_block',
+      '#url' => $config['ent_url'],
+      ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+
+    $form['ent_url'] = [
+      '#type' => 'link',
+      '#title' => $this->t('URL of ENT platform'),
+      '#description' => $this->t('Type the URL of the ENT platform. Must start with https://'),
+      '#default_value' => isset($config['ent_url']) ? $config['ent_url'] : '',
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['ent_url'] = $values['ent_url'];
+    $this->configuration['ent_label'] = $values['ent_label'];
+  }
+}
