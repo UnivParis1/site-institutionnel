2a2dd966c80e16b54071263be050de2c0f39742d
lhelmany <Liliana.Helmany@univ-paris1.fr>
2019-08-05 15:14:41 +0200

    Remove old PARIS1 theme

--- web/themes/custom/PARIS12018/js/tabs.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $( 'ul.nav.nav-tabs  a' ).click( function ( e ) {
-    e.preventDefault();
-  } );
-
-  // changement des Tabs en Accordions
-  fakewaffle.responsiveTabs( [ 'xs', 'sm' ] );
-} )( jQuery );
-
