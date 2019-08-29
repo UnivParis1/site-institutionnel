3dbecd586f94413f93791eb566ef164b5bba33b8
Carol <carole@ebizproduction.com>
2019-08-29 10:50:10 +0300

    header mobile

--- web/themes/custom/p1ps2019/js/src/main.js
+++ web/themes/custom/p1ps2019/js/src/main.js
@@ -63,13 +63,13 @@
 			.wrapInner('<span class="visually-hidden"></span>')
 			.append($('.language-switcher-language-url .language-link.is-active').attr('hreflang'))
 			.click(function(){
-        $(this).addClass('overlay-show');
         if($(this).next('ul.overlay-show').length > 0){
           overlayClose();
         }
         else{
           overlayClose();
           $(this).next('ul').addClass('overlay-show');
+          $('.language-switcher-language-url .block-title').addClass('overlay-show');
         }
 			});
 		}
