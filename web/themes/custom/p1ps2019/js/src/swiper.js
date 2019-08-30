d584df22755aa927a71800289ec360d611da9959
Carol <carole@ebizproduction.com>
2019-08-30 11:06:38 +0300

    bp view

--- web/themes/custom/p1ps2019/js/src/swiper.js
+++ web/themes/custom/p1ps2019/js/src/swiper.js
@@ -10,8 +10,8 @@
   				var mySwiper = new Swiper('.swiper-container', {
   					slidesPerView: 3,
             spaceBetween: 20,
-            slidesOffsetBefore: (screen.width - 1000) / 2,
-  					slidesOffsetAfter: (screen.width - 1000) / 2,
+            slidesOffsetBefore: (screen.width - 1300) / 2,
+  					slidesOffsetAfter: (screen.width - 1300) / 2,
             navigation: {
               nextEl: '.swiper-button-next',
               prevEl: '.swiper-button-prev',
