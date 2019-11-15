e226c505c08bda129078568cbe8dc4c2d2643edb
Carol <carole@ebizproduction.com>
2019-11-15 14:44:31 +0200

    swiper

--- web/themes/custom/p1ps2019/js/src/swiper.js
+++ web/themes/custom/p1ps2019/js/src/swiper.js
@@ -35,6 +35,7 @@
         if($('#swiper-carousel').length > 0){
   				var carouselSwiper = new Swiper('#swiper-carousel', {
   					slidesPerView: 1,
+            spaceBetween: 97,
             pagination: {
   				    el: '.swiper-pagination',
   				    type: 'bullets',
