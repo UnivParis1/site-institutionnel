ed123d454626179b0f84fad806fcc84e7f61e472
carole <carole@ebizproduction.com>
2021-01-25 12:19:55 +0200

    cpmment js

--- web/themes/custom/p1ps2019/js/src/main.js
+++ web/themes/custom/p1ps2019/js/src/main.js
@@ -110,21 +110,55 @@
     });
   }
 
-  var replaceUrlIframe = false;
+  // var replaceUrlIframe = false;
+  // var player;
+  //
+  // function onYouTubePlayerAPIReady() {
+  //   // create the global player from the specific iframe (#video)
+  //   player = new YT.Player('video', {
+  //       events: {
+  //           // call this function when player is ready to use
+  //           'onReady': onPlayerReady
+  //       }
+  //   });
+  // }
+
+  // function onPlayerReady(event) {
+  //     // bind events
+  //     var playButton = document.getElementById("play-button");
+  //     playButton.addEventListener("click", function() {
+  //         player.playVideo();
+  //     });
+  //
+  //     var pauseButton = document.getElementById("pause-button");
+  //     pauseButton.addEventListener("click", function() {
+  //         player.pauseVideo();
+  //     });
+  //
+  //     var stopButton = document.getElementById("stop-button");
+  //     stopButton.addEventListener("click", function() {
+  //         player.stopVideo();
+  //     });
+  // }
 
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
 
-      if($('.media-oembed-content').length > 0){
-        if(replaceUrlIframe == false){
-          setTimeout(function(){
-            console.log('iframe');
-            var srcIframe = $('.media-oembed-content').contents().find('iframe').attr('src') + '?enablejsapi=1&html5=1';
-            $('.media-oembed-content').contents().find('iframe').attr('src',srcIframe);
-            replaceUrlIframe = true;
-           }, 100);
-        }
-      }
+      // if($('.media-oembed-content').length > 0){
+      //   if(replaceUrlIframe == false){
+      //     setTimeout(function(){
+      //       console.log('iframe');
+      //       var srcIframe = $('.media-oembed-content').contents().find('iframe').attr('src') + '?enablejsapi=1&html5=1';
+      //       $('.media-oembed-content').contents().find('iframe').attr('src',srcIframe);
+      //       replaceUrlIframe = true;
+      //      }, 100);
+      //   }
+      //   // Inject YouTube API script
+      //   var tag = document.createElement('script');
+      //   tag.src = "https://www.youtube.com/player_api";
+      //   var firstScriptTag = document.getElementsByTagName('script')[0];
+      //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      // }
 
       $(document, context).once('foundation-init').each(function() {
         $(document).foundation();
