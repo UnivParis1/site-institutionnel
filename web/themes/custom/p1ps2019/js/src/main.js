53c053250b903679359690e4aa3e6fc3afa14bb4
rouaida <rouaida@ebizproduction.com>
2021-02-18 17:18:12 +0200

    video home

--- web/themes/custom/p1ps2019/js/src/main.js
+++ web/themes/custom/p1ps2019/js/src/main.js
@@ -131,22 +131,22 @@
       // }
 
       $('#play-button').click(function(){
-        var videoURL = $('#VdoID').attr('src'),
-        dataplay = $('#VdoID').attr('data-play');
+        var videoURL = $('.media-oembed-content').attr('src'),
+        dataplay = $('.media-oembed-content').attr('data-play');
     
         //for check autoplay
         //if not set autoplay=1
         if(dataplay == 0 ){
-            $('#VdoID').attr('src',videoURL+'?autoplay=1');
-            $('#VdoID').attr('data-play',1);
+            $('.media-oembed-content').attr('src',videoURL+'?autoplay=1');
+            $('.media-oembed-content').attr('data-play',1);
          }
          else{
-            var videoURL = $('#VdoID').attr('src');
+            var videoURL = $('.media-oembed-content').attr('src');
             videoURL = videoURL.replace("?autoplay=1", "");
-            $('#VdoID').prop('src','');
-            $('#VdoID').prop('src',videoURL);
+            $('.media-oembed-content').prop('src','');
+            $('.media-oembed-content').prop('src',videoURL);
     
-            $('#VdoID').attr('data-play',0);
+            $('.media-oembed-content').attr('data-play',0);
          }
     
     });
