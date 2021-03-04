6761a01484aa90a8f87be6d25f9f97e0966da88e
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2021-03-04 17:10:02 +0100

    Debug queue

--- web/modules/custom/up1_pages_personnelles/src/Controller/WsGroupsController.php
+++ web/modules/custom/up1_pages_personnelles/src/Controller/WsGroupsController.php
@@ -408,7 +408,8 @@ class WsGroupsController extends ControllerBase {
       $data[] = $this->selectFeUsers($user['uid']);
     }
     \Drupal::logger('up1_pages_personnelles')->info("Count Fe Users : " . count($data));
-    
+    \Drupal::logger('up1_pages_personnelles')->info("First Fe User : " . print_r($data[0],1));
+
     $queue = $this->queueFactory->get('up1_typo3_data_queue');
     foreach ($data as $datum) {
       $queue->createItem($datum);
