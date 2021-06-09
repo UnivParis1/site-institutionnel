b05c57bb4d7ab387752e01c2aa9e447aea2ea488
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2021-06-09 14:24:30 +0200

    goto 404 after unsupported browser message

--- web/modules/custom/up1_pages_personnelles/js/unsupported-browsers.js
+++ web/modules/custom/up1_pages_personnelles/js/unsupported-browsers.js
@@ -3,11 +3,11 @@
     attach: function (context, settings) {
       window.onload = function(){
         if (window.NodeList && !NodeList.prototype.forEach) {
-          document.location.href = "https://pantheonsorbonne.fr/pages-persos-unsupported-browsers";
           console.log('unsupported browser');
           var x = document.getElementById('node-page-personnelle-edit-form');
           x.setAttribute('style', 'display:block');
           alert('Votre navigateur ne prend pas en charge certaines fonctionnalités du site. Veuillez le mettre à jour ou changer de navigateur. ');
+          document.location.href = "https://pantheonsorbonne.fr/pages-persos-unsupported-browsers";
         }
       }
     }
