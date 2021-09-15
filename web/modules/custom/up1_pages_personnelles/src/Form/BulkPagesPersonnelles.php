b60815d9ec6f19aaf76de5a0b87e0cd6e88db509
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2021-09-15 12:18:59 +0200

    Automatic creation of page perso for new ecd

--- web/modules/custom/up1_pages_personnelles/src/Form/BulkPagesPersonnelles.php
+++ web/modules/custom/up1_pages_personnelles/src/Form/BulkPagesPersonnelles.php
@@ -44,7 +44,6 @@ class BulkPagesPersonnelles extends ConfigFormBase
     $form['uid_ldap'] = [
       '#type' => 'textarea',
       '#title' => $this->t('uid ldap'),
-      '#required' => TRUE,
       '#default_value' => implode(PHP_EOL, $config->get('uid_ldap')),
       '#description' => $this->t('Enter one uid per line.'),
     ];
