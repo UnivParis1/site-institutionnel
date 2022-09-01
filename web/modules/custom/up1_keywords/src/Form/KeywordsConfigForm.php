490c4975ec7d9ebedb8cd6f3a32088683689bfc9
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2022-09-01 14:36:02 +0200

    Tuesday #2618 - Highlighted keywords

--- web/modules/custom/up1_keywords/src/Form/KeywordsConfigForm.php
+++ web/modules/custom/up1_keywords/src/Form/KeywordsConfigForm.php
@@ -10,12 +10,15 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class KeywordsConfigForm extends ConfigFormBase {
 
+  protected $additionnal_rows = 0;
+
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames(): array
+  {
     return [
-      'up1_keywords.keywordsconfig',
+      'up1_keywords.settings',
     ];
   }
 
@@ -29,16 +32,85 @@ class KeywordsConfigForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('up1_keywords.keywordsconfig');
-    $form['url_resultat_de_recherche'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('URL de la vue de résultat de recherche'),
-      '#description' => $this->t('Doit être du type "https://URL-de-la-page/de-recherche?identifiant-du-filtre-de-recherche="'),
-      '#maxlength' => 64,
-      '#size' => 64,
-      '#default_value' => $config->get('url_resultat_de_recherche'),
+  public function buildForm(array $form, FormStateInterface $form_state): array
+  {
+    $config = $this->config('up1_keywords.settings');
+    $existing_keywords = $config->get('keywords_links');
+    $nb_keywords = count($existing_keywords);
+
+    $form['container'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'keywords-form-container']
     ];
+    if ($this->additionnal_rows < 5) {
+      $form['container']['actions'] = [
+        '#type' => 'actions'
+      ];
+      $form['container']['actions']['add_item'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Add keyword link'),
+        '#submit' => ['::add_keyword_item'],
+        '#ajax' => [
+          'callback' => '::add_keyword_item_ajax_callback',
+          'wrapper' => 'keywords-form-container',
+        ],
+      ];
+    }
+    $form['container']['keywords_links'] = [
+      '#type' => 'table',
+      '#tabledrag' => [
+        [
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'table-sort-weight',
+        ],
+      ],
+    ];
+    for ($i = 0; $i < $nb_keywords; $i++) {
+      $form["container"]['keywords_links'][$i]['#attributes']['class'][] = 'draggable';
+      $form["container"]["keywords_links"][$i]["url"] = [
+        '#type' => "url",
+        '#title' => $this->t("URL"),
+        '#default_value' => $existing_keywords[$i]["uri"],
+      ];
+      $form["container"]["keywords_links"][$i]["text"] = [
+        '#type' => "textfield",
+        '#title' => $this->t("Link text"),
+        '#default_value' => $existing_keywords[$i]["title"],
+      ];
+      $form["container"]['keywords_links'][$i]['weight'] = [
+        '#type' => 'weight',
+        '#title' => $this->t("Weight"),
+        '#title_display' => 'invisible',
+        '#default_value' => $i,
+        '#attributes' => [
+          'class' => ['table-sort-weight',]
+        ],
+      ];
+    }
+
+    for($i=0; $i < $this->additionnal_rows; $i++){
+      $form["container"]['keywords_links'][$nb_keywords+$i]['#attributes']['class'][] = 'draggable';
+      $form["container"]["keywords_links"][$nb_keywords+$i]["url"] = [
+        '#type' => "url",
+        '#title' => $this->t("URL"),
+      ];
+      $form["container"]["keywords_links"][$nb_keywords+$i]["text"] = [
+        '#type' => "textfield",
+        '#title' => $this->t("Link text"),
+      ];
+      $form["container"]['keywords_links'][$nb_keywords+$i]['weight'] = [
+        '#type' => 'weight',
+        '#title' => $this->t("Weight"),
+        '#title_display' => 'invisible',
+        '#default_value' => $nb_keywords+$i+1,
+        '#attributes' => [
+          'class' => ['table-sort-weight',]
+        ],
+      ];
+    }
+
+   $form_state->setCached(false);
     return parent::buildForm($form, $form_state);
   }
 
@@ -53,11 +125,34 @@ class KeywordsConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-
-    $this->config('up1_keywords.keywordsconfig')
-      ->set('url_resultat_de_recherche', $form_state->getValue('url_resultat_de_recherche'))
+    $values = $form_state->getValues()['keywords_links'];
+    \Drupal::logger('up1_keywords_values')->info(print_r($values,1));
+    $list = [];
+    foreach($values as $key => $value){
+      $list[] = ["uri" => $value['url'], "title" => $value['text']];
+    }
+    $this->config('up1_keywords.settings')
+      ->set('keywords_links', $list)
       ->save();
+    \Drupal::messenger()->addMessage($this->t("Keywords element(s) saved"));
   }
 
+  /**
+   * Function add_keyword_item_ajax_callback().
+   *
+   * @returns array $form.
+   */
+  public function add_keyword_item_ajax_callback($form, $form_state) {
+    return $form['container'];
+  }
+
+  /**
+   * Function add_keyword_item
+   */
+  public function add_keyword_item(array &$form, FormStateInterface $form_state) {
+    if ($this->additionnal_rows < 6) {
+      $this->additionnal_rows++;
+      $form_state->setRebuild();
+    }
+  }
 }
