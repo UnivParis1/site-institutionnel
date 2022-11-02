7a9b1e2560806de66f1f14956ed29aad96f52543
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2022-11-02 14:54:11 +0100

    Error naming classes

--- web/modules/custom/up1_search_api/src/Plugin/search_api/processor/AggregatedDateField.php
+++ web/modules/custom/up1_search_api/src/Plugin/search_api/processor/AggregatedDateField.php
@@ -7,6 +7,7 @@ use Drupal\search_api\Utility\Utility;
 use Drupal\search_api\Processor\ProcessorPluginBase;
 use Drupal\search_api\Datasource\DatasourceInterface;
 use Drupal\search_api\SearchApiException;
+use Drupal\up1_search_api\Plugin\search_api\processor\Property;
 
 /**
  * * Adds customized aggregations of existing fields to the index.
