abae607fc0e140762ccd533caa7c0057ab01e9b7
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2023-06-23 10:51:00 +0200

    delete useless code

--- web/modules/custom/up1_global/src/Service/CentresService.php
+++ web/modules/custom/up1_global/src/Service/CentresService.php
@@ -28,16 +28,7 @@ class CentresService {
    *   The base URL.
    */
   public function getWebServiceUrl() {
-    //$protocol = $this->settings->get('webservice_centres.protocol');
-    //$hostname = $this->settings->get('webservice_centres.hostname');
-    /*if (!isset($hostname) || empty($hostname)) {
-      \Drupal::logger('up1_global')
-        ->error('You must define the hostname of the web service');
-      return FALSE;
-    }
-    else {*/
       return "https://ws-centres.univ-paris1.fr/new_liste_centres_up1.json";
-    //}
   }
 
   public function getCentresJson($url) {
@@ -60,8 +51,6 @@ class CentresService {
     if (!empty($dataArray)) {
       $key = array_search($code, array_column($dataArray, 'code'));
       $centre = $dataArray[$key];
-      //$protocol = \Drupal::config('up1.settings')->get('webservice_centres.protocol');
-      //$path = \Drupal::config('up1.settings')->get('webservice_centres.images_path');
       $url_images = "https://ws-centres.univ-paris1.fr/images/";
       $file = "$url_images$code.jpg";
       if ($code == "0011_B") $file = $url_images . "0011_A.jpg";
@@ -70,14 +59,7 @@ class CentresService {
         $centre['image_path'] = $file;
       }
       else {
-        $file = "$url_images$code.png";
-        $file_headers = @get_headers($file);
-        if (!empty($file_headers) && $file_headers[0] == 'HTTP/1.1 200 OK') {
-          $centre['image_path'] = $file;
-        }
-        else {
-          $centre['image_path'] = file_create_url($url_images . "default_white.jpg");
-        }
+        $centre['image_path'] = file_create_url($url_images . "default_white.jpg");
       }
     }
 
