3111de2734d3e94b7d5837adb48dec8240985a80
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2024-01-08 15:32:04 +0100

    Change sn & givenName to displayName in page perso listings

--- web/modules/custom/up1_pages_personnelles/src/WsGroupsService.php
+++ web/modules/custom/up1_pages_personnelles/src/WsGroupsService.php
@@ -53,7 +53,7 @@ class WsGroupsService implements WsGroupsServiceInterface {
     $request = $this->getRequest($affiliation);
 
     $params = [
-      'attrs' => 'sn,givenName,labeledURI,employeeType,supannEntiteAffectation,eduPersonPrimaryAffiliation,supannListeRouge,supannConsentement'
+      'attrs' => 'sn,givenName,displayName,labeledURI,employeeType,supannEntiteAffectation,eduPersonPrimaryAffiliation,supannListeRouge,supannConsentement'
     ];
     if (!empty($trombi_settings)) {
       if ($trombi_settings['supannRole']) {
