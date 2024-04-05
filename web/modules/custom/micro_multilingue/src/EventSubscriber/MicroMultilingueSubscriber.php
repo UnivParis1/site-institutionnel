ba9efe5d3d88ea17da6e28cc7a45b30f983f0151
flocondetoile <fabrice.scoyer@flocondetoile.fr>
2024-04-05 21:55:00 +0200

    fix multilingue for micro site if only one language available (not fr)

--- web/modules/custom/micro_multilingue/src/EventSubscriber/MicroMultilingueSubscriber.php
+++ web/modules/custom/micro_multilingue/src/EventSubscriber/MicroMultilingueSubscriber.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\micro_multilingue\EventSubscriber;
 
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\micro_site\Entity\SiteInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Drupal\Console\Bootstrap\Drupal;
 use Drupal\Core\Language\LanguageManagerInterface;
@@ -60,8 +62,46 @@ class MicroMultilingueSubscriber implements EventSubscriberInterface {
   public function onKernelRequest(RequestEvent $event) {
     $request = $event->getRequest();
 
+    // If we've got an exception, nothing to do here.
+    if ($request->get('exception') != NULL) {
+      return;
+    }
+
     $default_language = $this->languageManager->getDefaultLanguage();
+    $current_language = $this->languageManager->getCurrentLanguage();
+    /** @var \Drupal\micro_site\SiteNegotiatorInterface $negotiator */
+    $negotiator = \Drupal::service('micro_site.negotiator');
     if(!$this->languageValidator->isAvailableLanguage()) {
+      $site = $negotiator->getActiveSite();
+      // Check on the site the best default language available.
+      if ($site instanceof SiteInterface) {
+        $site_language = $site->language();
+        if ($site_language instanceof LanguageInterface) {
+          $default_language = $site_language;
+        }
+        else {
+          foreach ($site->get('active_language') as $language) {
+            $default_language = $language->entity;
+            break;
+          }
+        }
+        // The default language fallback is still the current language.
+        // Try to get the first available language of the site.
+        if ($default_language->getId() === $current_language->getId()) {
+          foreach ($site->get('active_language') as $language) {
+            $default_language = $language->entity;
+            break;
+          }
+        }
+      }
+
+      // The default language is still the current language. Current language not
+      // available for the mini site. We can't do anything anymore. Stop.
+      if ($default_language->getId() === $current_language->getId()) {
+        // In theory we should never enter here.
+        return;
+      }
+
       $route_match = RouteMatch::createFromRequest($request);
       $route_name = $route_match->getRouteName();
       $parameters = $route_match->getRawParameters()->all();
--- web/modules/custom/micro_multilingue/src/LanguageValidator.php
+++ web/modules/custom/micro_multilingue/src/LanguageValidator.php
@@ -4,6 +4,7 @@ namespace Drupal\micro_multilingue;
 use Drupal\Console\Bootstrap\Drupal;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\micro_site\Entity\SiteInterface;
 use Drupal\micro_site\SiteNegotiatorInterface;
 use Drupal\micro_multilingue\LanguageValidatorInterface;
 
@@ -23,23 +24,26 @@ class LanguageValidator implements LanguageValidatorInterface
   }
 
   public function isAvailableLanguage (){
-    $default_language = $this->languageManager->getDefaultLanguage();
     $active_language = $this->languageManager->getCurrentLanguage();
-    $availableLanguage = [];
-    if($this->negotiator->isHostUrl()){
-      $availableLanguage = array_values(\Drupal::config('micro_multilingue.settings')->get('host_active_language'));
-    }
-    else {
-      $site = $this->negotiator->getActiveSite();
-      if(!empty($site)) {
-        foreach ($site->get('active_language') as $language) {
-          $availableLanguage[] =$language->entity->getID();
-        }
-      }
-    }
-    if(empty($availableLanguage)) {
+    $availableLanguage = $this->getAvailableLanguageIds();
+    if (empty($availableLanguage)) {
       $availableLanguage[] = $active_language->getID();
     }
     return in_array($active_language->getID(), $availableLanguage, true);
+  }
+
+  public function getAvailableLanguageIds() {
+    $availableLanguage = [];
+    $site = $this->negotiator->getActiveSite();
+    if ($site instanceof SiteInterface) {
+      foreach ($site->get('active_language') as $language) {
+        $availableLanguage[] = $language->entity->getID();
+      }
     }
+    elseif($this->negotiator->isHostUrl()){
+      $availableLanguage = array_values(\Drupal::config('micro_multilingue.settings')->get('host_active_language'));
+    }
+    return $availableLanguage;
+  }
+
 }
