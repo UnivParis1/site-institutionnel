2639634406610e82a592520dad089477c61dc630
Liliana HELMANY <Liliana.Helmany@univ-paris1.fr>
2024-06-25 14:17:02 +0200

    Module custom admin extend

---
+++ web/modules/custom/up1_admin_extend/test.sql
@@ -0,0 +1,52 @@
+43689 = 43689
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value)
+
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'acteurs_principaux',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'adresse_de_l_organisme_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'adresse_evenement_organisme',0,'5 RUE DU TAGE 75013 PARIS');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'avez_vous_des_precisions_a_apporter',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'avez_vous_des_questions_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'besoins_materiels_sonorisation_videoprojecteur_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'courriel_directeur_de_societe',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'courriel_evenement_organisme',0,'	hlabchiri@hotmail.com');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'courriel_regisseur',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'courriel_regisseur_evenement_organisme',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'courriel_tournage',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'dates_et_horaires_de_l_evenement',0,'SAMEDI 6 JUILLET 2024 / 15H-17H');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'date_s_ou_periode_s_de_tournage_envisagee_s_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'diffuseur',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'directeur_de_societe_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'evenement_payant',0,'Oui');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'informations_complementaires_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'lieu_s_souhaite_s_',0,'Sorbonne');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'lieu_s_souhaite_s_',1,'Centre Panthéon');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'lieu_s_souhaite_s_',2,'Institut de géographie');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nature_de_la_demande_evement_organisme',0,'Formation');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_de_figurants_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_de_journee_s_de_tournage_envisagee_s_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_de_participants_sur_site',0,30
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_de_personne_de_l_equipe_technique_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_d_acteurs_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nombre_d_espaces_souhaites',0,1);
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nom_des_intervenants',0,'LISTE');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nom_du_contact_si_different_du_responsable_de_l_evenement',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'nom_du_directeur_de_la_societe',0,'LABCHIRI');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'objet_de_l_evenement_themes_sujets_objectifs_abordes_',0,'COURS POUR ADULTES EN HISTOIRE DE L ART ');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'realisateur',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'reference_de_l_organisme_siret_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'reference_de_l_organisme_siret_evenement_organisme',0,'913 430 815 00021');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'regisseur_ou_contact_',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'regisseur_ou_contact_evenement_organisme',0,'LABCHIRI');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'societe_de_production',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'societe_ou_association_evenement_organisme',0,'LABCHIRI');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'synopsis',0,'');
+INSERT INTO dpl_webform_submission_data (webform_id,sid,name,delta,value) VALUES('57_formulaire_locations',43689,'telephone_',0,'');
+57_formulaire_locations	43689	telephone_directeur_de_societe		0
+57_formulaire_locations	43689	telephone_evenement_organisme		0	0634497903
+57_formulaire_locations	43689	telephone_regisseur_evenement_organisme		0
+57_formulaire_locations	43689	telephone_regisseur_ou_contact		0
+57_formulaire_locations	43689	titre_du_projet		0
+57_formulaire_locations	43689	type		0
+57_formulaire_locations	43689	un_tournage		0	UN EVENEMENT
+57_formulaire_locations	43689	_salles_souhaitees_		0	Salle(s) de classe
+
