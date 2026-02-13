6348027dec85836e708d8f6c35795370b66eeade
lhelmany <Liliana.Helmany@univ-paris1.fr>
2026-02-13 16:16:58 +0100

    Hotfix - trace erreurs json

--- web/modules/custom/up1_global/src/Form/CentresUp1Form.php
+++ web/modules/custom/up1_global/src/Form/CentresUp1Form.php
@@ -81,23 +81,24 @@ class CentresUp1Form extends FormBase {
   }
 
   public function getCentresJson() {
-    //$protocol = \Drupal::config('up1.settings')->get('webservice_centres.protocol');
-    //$hostname = \Drupal::config('up1.settings')->get('webservice_centres.hostname');
     $url = "https://ws-centres.univ-paris1.fr/new_liste_centres_up1.json";
 
     $json = file_get_contents($url);
-    $dataArray = json_decode($json, TRUE);
+    if ($json) {
+	    $dataArray = json_decode($json, TRUE);
+	    if (json_last_error() !== JSON_ERROR_NONE) {
+        \Drupal::logger('up1_global')->error("Erreur liste centres : " . json_last_error_msg());
+      }
+    }
+    else {
+      \Drupal::logger('up1_global')->error('Erreur json liste centres');
+    }
 
     return $dataArray;
   }
 
   public function getCentreImage($code) {
-    //$protocol = \Drupal::config('up1.settings')->get('webservice_centres.protocol');
-    //$path = \Drupal::config('up1.settings')->get('webservice_centres.images_path');
     $url = "https://ws-centres.univ-paris1.fr/new_liste_centres_up1.json";
-
-    $image_path = "";
-
     $file = "$url$code.jpg";
     $file_headers = @get_headers($file);
 
@@ -204,6 +205,10 @@ class CentresUp1Form extends FormBase {
         $options[$key] = $item;
       }
     }
+    else {
+      \Drupal::logger('up1_global')->error('No centres found');
+    }
+
 
     return $options;
   }
